1f69c16cf9f8907ebee40f5edcbee06c708dc309
Diego Alejandro Heredia Franco <dherediaf@unal.edu.co>
2019-09-04 12:52:57 -0500

    cosas

---
+++ clase0/holamundo.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(void){
+
+  double x=1.0e+6;
+
+  std::cout << "Hola, he vuelto!" << std::endl;
+  // \n cumple la misma funcion que endl
+
+  std::cout << x << "\n";
+
+  
+  
+  return 0;
+
+}
