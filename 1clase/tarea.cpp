65091106c7c0f4587c21189bd015ef20dd7da572
Diego Alejandro Heredia Franco <dherediaf@unal.edu.co>
2019-09-10 13:54:11 -0500

    hecho

--- 1clase/tarea.cpp
+++ 1clase/tarea.cpp
@@ -30,11 +30,16 @@ int unovoble(void){
 
   double a=1.0;
   double b=0.0;
-  for(int i=1;i<99;i++){
-    a = a*1.3e3;
-  }
 
-  printf("%20.20e\n",a);
+  while(a != a*2){
+    b=a;
+    a=a*2;
+  }
+  // for(int i=1;i<99;i++){
+  //   a = a*1.3e3;
+  // }
+
+  printf("%20.20e\n",b);
   
   a=1.0;
   
@@ -54,11 +59,16 @@ int unovat(void){
 
   float a=1.0;
   float b=0.0;
-  for(int i=1;i<13;i++){
-    a = a*1.3e3;
-  }
 
-  printf("%20.16e\n",a);
+  while(a != a*2){
+    b=a;
+    a=a*2;
+  }
+  // for(int i=1;i<13;i++){
+  //   a = a*1.3e3;
+  // }
+
+  printf("%20.16e\n",b);
   
   a=1.0;
   
