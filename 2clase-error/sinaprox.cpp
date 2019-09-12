ea25834066caf70d102b5f60aac950a6e8482db8
Diego Alejandro Heredia Franco <dherediaf@unal.edu.co>
2019-09-11 23:05:53 -0500

    corregido y listo

--- 2clase-error/sinaprox.cpp
+++ 2clase-error/sinaprox.cpp
@@ -6,16 +6,20 @@ double sen(const double &x, const int &lim);
 
 int main(void)
 {
-  int N=5;
-  double x=0.5;
+  int N=10;
+  double x=0.23;
   double se=0;
   double dsin=0;
   std::ofstream fout("datos.txt");
-  for(int ii=1; ii<=N; ii++)
+  for(x=0.1; x<8.0; x+=0.1)
     {
-      se=sen(x,ii);
-      dsin = std::abs(se-std::sin(x));
-      fout << ii << "  " << se << "  " << dsin << "\n";
+      fout << x << "\t" << sen(x,N) << "\t" << std::sin(x) << "\n";
+      // for(int ii=1; ii<=N; ii++)
+      // 	{
+      // 	  se=sen(x,ii);
+      // 	  dsin = std::abs(se-std::sin(x))/std::sin(x);
+      // 	  fout << ii << "\t" << se << "\t" << std::sin(x) << "\t" <<dsin << "\n";
+      // 	}
     }
   fout.close();
   return 0;
@@ -25,10 +29,10 @@ double sen(const double &x, const int &lim)
 {
   double term=x;
   double res=0;
-  for(int i=2; i<=lim; i++)
+  for(int i=1; i<=lim; i++)
     {
-      term=term*(-1)*x*x/((2*i)*(2*i+1));
       res += term;
+      term=term*(-1)*x*x/((2*i)*(2*i+1));
     }
   return res;
 }
