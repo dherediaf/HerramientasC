ae241ffb8ddca82819347a13d6ce56d8e04930a2
diego alejandro heredia franco <dherediaf@unal.edu.co>
2019-09-30 14:15:09 -0500

    nada

---
+++ 4clase-libmath/practica/cubic-distri.cpp
@@ -0,0 +1,45 @@
+#include <random>
+#include <iostream>
+#include <vector>
+
+
+int histocubic(const double xmin,const double xmax, const double dx, const int samples, const int seed);
+
+int main()
+{
+  const int seed=1;
+  const double xmin=1.0;
+  const double xmax=2.0;
+  const double dx=0.01;
+  const int samples=10000;
+
+  histocubic(xmin,xmax,dx,samples,seed);
+  return 0;
+}
+
+int histocubic(const double xmin, const double xmax, const double dx, const int samples, const int seed)
+{
+  const double ymin=std::log(xmin);
+  const double ymax=std::log(xmax);
+  std::mt19937 gen(seed); //mersen twister
+  std::uniform_real_distribution<> dis(ymin, ymax);
+  
+  const int nbins=(xmax-xmin)/dx;
+  std::vector<double> histograma(nbins);
+  
+  for(int n=0; n<samples; n++)
+    {
+      double r=std::exp(dis(gen));
+      int idx=int(r/dx);
+      if(0<=idx and idx<nbins)
+	{
+	  histograma[idx]+=1;
+	}
+    }
+
+  for(int i=0; i<nbins; i++)
+    {
+      std::cout << xmin + i*dx << "\t" << histograma[i]/(samples*dx) << "\n";
+    }
+  return 0;
+}
---
+++ 4clase-libmath/practica/plot.gp
@@ -0,0 +1,3 @@
+set xlabel "dx"
+set ylabel "suma"
+plot "datos.txt" u 1:2 w lp
