252b37067e0a360ac5d7724a08a43e3e6c785c27
Diego Alejandro Heredia Franco <dherediaf@unal.edu.co>
2019-10-23 13:00:55 -0500

    benchmark, perf y gprog

--- 8clase-profiling/cache_lines.c
+++ 8clase-profiling/cache_lines.c
@@ -2,79 +2,70 @@
 #include <stdlib.h>
 #include <time.h>
 
-void allocate(int n, float ***a){
-  
-  b = malloc(n*sizeof(float **));
-  for (int i=0; i<n; i++){
-    b[i] = malloc(n*sizeof(float*));
-    for (int j=0; j<n; j++)
-      b[i][j] = malloc(n*sizeof(float));
-  }
-}
+void f1(float *** x, int n);
+void f2(float *** x, int n);
+void f3(float *** x, int n);
 
-void kji(clock_t cputime1, clock_t cputime2, int n, float ***a){
-  
-  cputime1 = clock(); //cambia indices k j i
-  for (int k=0; k<n; ++k)
-    for (int j=0; j<n; ++j)
-      for (int i=0; i<n; ++i)
-        a[i][j][k] = 1.0;
+int main(){
+  const int n = 128;
+  clock_t cputime1, cputime2;
+  float ***a;
+  int i,j,k;
+
+  // Allocating memory for array/matrix
+  a = malloc(n*sizeof(float **));
+  for (i=0; i<n; i++){
+    a[i] = malloc(n*sizeof(float*));
+    for (j=0; j<n; j++)
+      a[i][j] = malloc(n*sizeof(float));
+  }
+
+  cputime1 = clock();
+  f1(a, n);
   cputime2=clock() - cputime1;
   printf("Time with fast index inside: %lf\n", ((double)cputime2)/CLOCKS_PER_SEC);
 
-}
 
-void ijk(clock_t cputime1, clock_t cputime2, int n, float ***a){
-
-  cputime1 = clock(); //cambia indices i j k
-  for(int i=0; i<n; ++i)
-    for (int j=0; j<n; ++j)
-      for (int k=0; k<n; ++k)
-        a[i][j][k] = 2.3;
+  cputime1 = clock();
+  f2(a, n);
   cputime2=clock() - cputime1;
   printf("Time with fast index outside: %lf\n", ((double)cputime2)/CLOCKS_PER_SEC);
-}
 
-void ijkrev(clock_t cputime1, clock_t cputime2, int n, float ***a){
+  cputime1 = clock();
+  f3(a, n);
+  cputime2=clock() - cputime1;
+  printf("Time with fast index outside substracting: %lf\n", ((double)cputime2)/CLOCKS_PER_SEC);
 
-    cputime1 = clock(); //cambia indices i j k, de n-1 hasta 0
-    for(int i=n-1; i>=0; --i)
-      for (int j=n-1; j>=0; --j)
-	for (int k=n-1; k>=0; --k)
-	  a[i][j][k] = 2.3;
-    cputime2=clock() - cputime1;
-    printf("Time with fast index outside substracting: %lf\n", ((double)cputime2)/CLOCKS_PER_SEC);
-  }
-
-void clean(int n, float ***a){
-  
-  for (int i=0; i<n; i++){
-    for (int j=0; j<n; j++)
+  // Clearing memory
+  for (i=0; i<n; i++){
+    for (j=0; j<n; j++)
       free(a[i][j]);
     free(a[i]);
   }
   free(a);
-}
-
-int main(){
-  const int n = 256; //Potencias de 2
-  clock_t cputime1, cputime2;
-  float ***a;
-
-  
-  allocate(n,a);// Allocating memory for array/matrix
-
-  a=b;
-
-  kji(cputime1,cputime2,n,a);
-
-  ijk(cputime1,cputime2,n,a);
-
-  ijkrev(cputime1,cputime2,n,a);
-
-  clean(n,a); // Clearing memory
 
   return 0;
 }
 
+void f1(float *** x, int n)
+{
+  for (int k=0; k<n; ++k)
+    for (int j=0; j<n; ++j)
+      for (int i=0; i<n; ++i)
+        x[i][j][k] = 1.0;
+}
+void f2(float *** x, int n)
+{
+  for(int i=0; i<n; ++i)
+    for (int j=0; j<n; ++j)
+      for (int k=0; k<n; ++k)
+        x[i][j][k] = 2.3;  
+}
 
+void f3(float *** x, int n)
+{
+  for(int i=n-1; i>=0; i--)
+    for (int j=n-1; j>=0; j--)
+      for (int k=n-1; k>=0; k--)
+        x[i][j][k] = 3.2;  
+}
